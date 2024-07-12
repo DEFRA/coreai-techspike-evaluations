a38938c5149c652c19408041e743999e2317a220
barrygeedefra <barry.gee@defra.gov.uk>
2024-07-12 09:11:12 +0100

    chore: Update app.py to optimize document loading and embedding process

--- app/app.py
+++ app/app.py
@@ -5,10 +5,9 @@ from langchain_community.llms import Ollama
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
-
-# import psycopg2
 from langchain_core.documents import Document
 from langchain_postgres.vectorstores import PGVector
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -20,12 +19,19 @@ base_url = os.getenv("LANGFUSE_HOST")
 # Initialize Ollama with the model name
 llm = Ollama(model=model)
 
-# Create document loader for a PDF in the ./docs directory
+
+# DOCUMENT LOADER --------------------------------------------------------------------------
+
+# # Create document loader for a PDF in the ./docs directory
 file_path = "./docs/defra.pdf"
 
 # Load and split the document
 loader = PyPDFLoader(file_path)
 pages = loader.load()
+
+
+
+# TEXT SPLITTER ----------------------------------------------------------------------------
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
 docs = text_splitter.split_documents(pages)
@@ -33,10 +39,14 @@ docs = text_splitter.split_documents(pages)
 # TEMP for local testing - added to reduce time for saving embeddings to PGVector
 # docs = docs[0:2]
 
+
+
+# CREATE AND STORE EMBEDDINGS ----------------------------------------------------------------------------
+
 # Create embeddings
 embeddings = OllamaEmbeddings(model='llama3')
 
-doc_vectors = embeddings.embed_documents([t.page_content for t in docs[:5]]) # update :1 to :5 to return 5 documents
+doc_vectors = embeddings.embed_documents([t.page_content for t in docs[:5]]) # update :1 to :5 - defines the number of documents to embed
 
 # Vectorstore
 connection_string = f"postgresql+psycopg://{os.getenv('POSTGRES_USERNAME')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
@@ -49,6 +59,10 @@ vectorstore = PGVector.from_documents(
     use_jsonb=True,
     async_mode=False,
 )
+
+
+
+# FIND SIMILAR VECTORS BASED ON THE QUERY ----------------------------------------------------------------
 
 # Query
 query = "What is the budget?"
