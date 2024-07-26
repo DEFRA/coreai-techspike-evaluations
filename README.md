4383c76d3f3b64228f01785e7de62c537aaf8ebb
barrygeedefra <barry.gee@defra.gov.uk>
2024-07-26 10:44:20 +0100

    Updates

--- README.md
+++ README.md
@@ -1,13 +1,68 @@
-# POC: GenAI Retrieval Augmented Generation (RAG) application with locally running models using Ollama, Langchain, Langfuse and Ragas
+# Langchain Retrieval Augmented Generation (RAG) application
+
+- This application includes a containerised Langchain RAG application.
+- Vector embeddings are stored in a locally running containerised PGVector database.
+- LLM inputs and outputs, traces and prompts are monitored in a locally running containerised Langfuse installation.
+- The application uses locally running LLMs made available via Ollama.
+
 
 ## Setup
+
+### Ollama
+
+Download and install Ollama
+
+https://ollama.com
+
+
+Download the required embedding model
+```
+ollama pull nomic-embed-text
+```
+
+Download and run the required LLM
+```
+ollama run phi3:mini
+```
+This should be the same model defined in the .env file 'MODEL_NAME'
+
+
+### Docker
+
+Next, run the application, vector database and Langfuse:
 
 In the root directory run 
 ```
 docker-compose up --build
 ```
 
-## Python3 Environment
+
+## Langfuse
+
+Once the Langfuse-server container is up and running;
+
+If this is the first time you have setup the application:
+- visit http://localhost:3000 in your browser
+- Create a new account
+- Click the 'New Project' button
+- Name the project 'coreai-techspike-evaluations'
+- Click 'Settings' in the left hand panel 
+- Click 'Create new API keys'
+- Copy the secret key and paste it into your .env file 'LANGFUSE_SECRET_KEY'
+- Copy the public key and paste it into your .env file 'LANGFUSE_PUBLIC_KEY'
+- Run ```docker compose down```
+- Run ```docker compose up --build```
+
+
+
+## PGVector
+
+Reference: 
+https://api.python.langchain.com/en/latest/vectorstores/langchain_postgres.vectorstores.PGVector.html#langchain_postgres.vectorstores.PGVector
+
+
+
+## Python3 Environment - For code hinting
 
 Create a new Python3 virtual environment
 ```
@@ -23,38 +78,3 @@ Install the required dependencies into the new Python3 virtual environment
 ```
 pip install -r requirements.txt
 ```
-
-## Ollama
-
-- Download and install Ollama from https://ollama.com
-- Run
-```
-ollama pull phi3:mini
-```
-```
-ollama pull nomic-embed-text
-```
-```
-ollama run phi3:mini
-```
-
-## Langfuse
-
-Once the Langfuse-server container is up and running;
-
-If this is the first time you have setup the application:
-- visit http://localhost:4000 in your browser
-- Create a new account
-- Click the 'New Project' button
-- Name the project 'coreai-techspike-evaluations'
-- Click 'Settings' in the left hand panel 
-- Click 'Create new API keys'
-- Copy the secret key and paste it into your .env file 'LANGFUSE_SECRET_KEY'
-- Copy the public key and paste it into your .env file 'LANGFUSE_PUBLIC_KEY'
-
-
-
-## PGVector
-
-Reference: 
-https://api.python.langchain.com/en/latest/vectorstores/langchain_postgres.vectorstores.PGVector.html#langchain_postgres.vectorstores.PGVector
--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-from dotenv import load_dotenv
 import os
 import uuid
+from dotenv import load_dotenv
 
 from langchain_community.llms import Ollama
 from langchain_community.document_loaders import PyPDFLoader
@@ -12,20 +12,42 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain.prompts import ChatPromptTemplate
 from langfuse.callback import CallbackHandler
 
-session_id = str(uuid.uuid4())
 
-langfuse_handler = CallbackHandler(
-  secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
-  public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
-  host=os.getenv("LANGFUSE_HOST"),
-  session_id=session_id
-)
+# Environment variables
 
 def load_environment_variables():
   load_dotenv()
+  
   model = os.getenv("MODEL_NAME")
-  base_url = os.getenv("LANGFUSE_HOST")
-  return model, base_url
+  host_service_url = os.getenv('HOST_SERVICE_URL')
+  secret_key=os.getenv("LANGFUSE_SECRET_KEY")
+  public_key=os.getenv("LANGFUSE_PUBLIC_KEY")
+  langfuse_host=os.getenv("LANGFUSE_HOST")
+  langfuse_tags_environment=os.getenv("LANGFUSE_TAGS_ENVIRONMENT")
+  langfuse_tags_application_version=os.getenv("LANGFUSE_TAGS_APPLICATION_VERSION")
+
+  return model, host_service_url, secret_key, public_key, langfuse_host, langfuse_tags_environment, langfuse_tags_application_version
+
+
+
+# Initialize CallbackHandler
+
+def initialize_callback_handler(secret_key, public_key, langfuse_host, langfuse_tags_environment, langfuse_tags_application_version, session_id):
+    langfuse_handler = CallbackHandler(
+      secret_key=secret_key,
+      public_key=public_key,
+      host=f"http://{langfuse_host}:3000",
+      tags=[
+        langfuse_tags_environment,
+        langfuse_tags_application_version
+      ],
+      session_id=session_id
+    )
+    return langfuse_handler
+
+
+
+# Load and split documents from the /app/docs directory
 
 def load_documents():
   current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -34,12 +56,12 @@ def load_documents():
   documents = []
 
   for pdf in pdfs:
-      file_path = os.path.join(docs_directory_path, pdf)
-      loader = PyPDFLoader(file_path)
-      pdf_docs = loader.load()
-      documents.extend(pdf_docs)
-      for doc in pdf_docs:
-          documents.append(doc)
+    file_path = os.path.join(docs_directory_path, pdf)
+    loader = PyPDFLoader(file_path)
+    pdf_docs = loader.load()
+    documents.extend(pdf_docs)
+    for doc in pdf_docs:
+        documents.append(doc)
 
   return documents
 
@@ -48,9 +70,12 @@ def split_documents(documents):
   docs = text_splitter.split_documents(documents)
   return docs
 
+
+
+# Create vector embeddings from the chunked documents and save into PGVector database
+
 def create_and_store_embeddings(docs):
-  embeddings = OllamaEmbeddings(base_url='http://localhost:11434', model='nomic-embed-text')
-  # doc_vectors = embeddings.embed_documents([t.page_content for t in docs])
+  embeddings = OllamaEmbeddings(model='nomic-embed-text', base_url=f"http://{os.getenv('HOST_SERVICE_URL')}")
 
   connection_string = f"postgresql+psycopg2://{os.getenv('POSTGRES_USERNAME')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
   collection_name = os.getenv('PGVECTOR_COLLECTION_NAME')
@@ -58,13 +83,17 @@ def create_and_store_embeddings(docs):
   vectorstore = PGVector.from_documents(
       embedding=embeddings,
       documents=docs,
-      connection=connection_string, 
+      connection=connection_string,
       collection_name=collection_name,
       use_jsonb=True,
       async_mode=False,
   )
 
   return vectorstore
+
+
+
+# Document retrieval
 
 def retrieve_relevant_documents(vectorstore, question):
   retriever = vectorstore.as_retriever(
@@ -75,12 +104,21 @@ def retrieve_relevant_documents(vectorstore, question):
   relevant_docs = retriever.invoke(question)
   return relevant_docs
 
-def initialize_model(model_name):
+
+
+# LLM 
+
+def initialize_model(model_name, host_service_url):
   model = Ollama(
       model=model_name,
+      base_url=f"http://{host_service_url}",
       temperature=2
   )
   return model
+
+
+
+# Prompt template
 
 def create_prompt_template():
   template = """Answer the question based on the following context: {context}
@@ -96,24 +134,37 @@ def create_prompt_template():
   )
   return prompt_template
 
+
+
+# Langchain chain
+
 def create_chain(prompt_template, model):
   chain = prompt_template | model | StrOutputParser()
   return chain
 
-def main(q="What is the Annual budget?"):
-  model_name, base_url = load_environment_variables()
-  question = q
+
+
+# Initiate the application
+
+def main(question = "What IS DEFRA?"):
+  session_id = str(uuid.uuid4())
+  model_name, host_service_url, secret_key, public_key, langfuse_host, langfuse_tags_environment, langfuse_tags_application_version  = load_environment_variables()
+  langfuse_handler = initialize_callback_handler(secret_key, public_key, langfuse_host, langfuse_tags_environment, langfuse_tags_application_version, session_id)
+
   documents = load_documents()
   docs = split_documents(documents)
+
   vectorstore = create_and_store_embeddings(docs)
   relevant_docs = retrieve_relevant_documents(vectorstore, question)
 
-  model = initialize_model(model_name)
+  model = initialize_model(model_name, host_service_url)
   prompt_template = create_prompt_template()
-  chain = create_chain(prompt_template, model)
-
-  result = chain.invoke({"context": relevant_docs, "question": question}, config={"callbacks": [langfuse_handler]})
-  print(result)
+  
+  config = {"callbacks": [langfuse_handler]}
+  chain = create_chain(prompt_template, model) 
+  result = chain.invoke({"context": relevant_docs, "question": question}, config=config)
+  
+  print('ANSWER: ', result)
 
 if __name__ == "__main__":
     main()
